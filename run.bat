f60373a257a54a578e1eb620cf658bdf98a76633
Aoife <aoifemcdonagh95@hotmail.com>
2021-01-14 11:35:15 +0000

    file to run gui from file explorer

---
+++ run.bat
@@ -0,0 +1,7 @@
+set PYTHONPATH=C:\OSGeo4W64\apps\qgis\python;%PYTHONPATH%
+set PYTHONPATH=C:\OSGeo4W64\apps\qgis\python\plugins;%PYTHONPATH%
+set PYTHONPATH=C:\OSGeo4W64\apps\Python37\lib\site-packages;%PYTHONPATH%
+
+set QT_QPA_PLATFORM_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\Qt5\plugins
+
+python C:\Users\Aoife\Documents\farmeye\qgis_scripts\gui.py
