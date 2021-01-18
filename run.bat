c4d92d0d4650728078bbdd1cf73b5780d96ec583
Aoife <aoifemcdonagh95@hotmail.com>
2021-01-18 13:29:10 +0000

    remove hard coded path to gui.py

--- run.bat
+++ run.bat
@@ -4,4 +4,4 @@ set PYTHONPATH=C:\OSGeo4W64\apps\Python37\lib\site-packages;%PYTHONPATH%
 
 set QT_QPA_PLATFORM_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\Qt5\plugins
 
-python C:\Users\Aoife\Documents\farmeye\qgis_scripts\gui.py
+python gui.py
